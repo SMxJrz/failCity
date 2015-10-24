8c688331c64eca3f47dd32b2bc5ffb299ac6143f
Stephen Mele <Smelecs@gmail.com>
2015-10-24 13:56:17 -0400

    more tests

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,7 +1,23 @@
 'use strict';
 
-angular.module('myApp.view1', [])
+angular.module('myApp.view1', [
+
+])
 
 .controller('View1Ctrl', [function() {
+        this.mult10 = function(number){
+            return number * 10;
+        };
 
+        this.div10 = function(number){
+            return number / 10;
+        };
+
+        this.reverse = function(arr){
+            var res = [];
+            for(var i = arr.length - 1; i >= 0; i--){
+                res.push(arr[i]);
+            }
+            return res;
+        }
 }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -12,5 +12,29 @@ describe('myApp.view1 module', function() {
       expect(view1Ctrl).toBeDefined();
     }));
 
-  });
+    it('should multiply by 10', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+          expect(ctrl.mult10(10)).toBe(100);
+        }));
+    });
+
+    it('should divide by 10', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+        expect(ctrl.div10(10)).toBe(1);
+      }));
+
+    it('should reverse', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+        expect(ctrl.reverse([1, 10, 100])).toEqual([100, 10, 1]);
+      }));
+
+    it('should multiply by 10', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.mult10(1230)).toBe(12300);
+    }));
+
+    it('should multiply by 10', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.reverse([])).toEqual([]);
+    }));
 });
