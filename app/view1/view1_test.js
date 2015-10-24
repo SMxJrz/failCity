a21f6e84dbefaad84dc994d05dcefd948c6a64c3
Stephen Mele <Smelecs@gmail.com>
2015-10-24 14:02:36 -0400

    get rid of stub tests

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -6,34 +6,53 @@ describe('myApp.view1 module', function() {
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
-    }));
-
-    it('should multiply by 10', inject(function($controller){
+    it('Test 1', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
           expect(ctrl.mult10(10)).toBe(100);
         }));
     });
 
-    it('should divide by 10', inject(function($controller){
+    it('Test 2', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
         expect(ctrl.div10(10)).toBe(1);
       }));
 
-    it('should reverse', inject(function($controller){
+    it('Test 3', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
         expect(ctrl.reverse([1, 10, 100])).toEqual([100, 10, 1]);
       }));
 
-    it('should multiply by 10', inject(function($controller){
+    it('Test 4', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
       expect(ctrl.mult10(1230)).toBe(12300);
     }));
 
-    it('should multiply by 10', inject(function($controller){
+    it('Test 5', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.reverse([])).toEqual([]);
+    }));
+
+    it('Test 6', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.mult10(9)).toBe(90);
+    }));
+
+    it('Test 7', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.div10(230)).toBe(23);
+    }));
+
+    it('Test 8', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.reverse([31, 3, 34])).toEqual([34, 3, 31]);
+    }));
+
+    it('Test 9', inject(function($controller){
+      var ctrl = $controller('View1Ctrl');
+      expect(ctrl.mult10(1230)).toBe(12300);
+    }));
+
+    it('Test 10', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
       expect(ctrl.reverse([])).toEqual([]);
     }));
