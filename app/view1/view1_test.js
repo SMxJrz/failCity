3415167aa8baa62b938281de16046728286393f2
Stephen Mele <Smelecs@gmail.com>
2015-10-24 14:08:55 -0400

    make all the tests fail

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -8,52 +8,52 @@ describe('myApp.view1 module', function() {
 
     it('Test 1', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-          expect(ctrl.mult10(10)).toBe(100);
+          expect(ctrl.mult10(10)).toBe(1000);
         }));
     });
 
     it('Test 2', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-        expect(ctrl.div10(10)).toBe(1);
+        expect(ctrl.div10(10)).toBe(10);
       }));
 
     it('Test 3', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-        expect(ctrl.reverse([1, 10, 100])).toEqual([100, 10, 1]);
+        expect(ctrl.reverse([1, 10, 100])).toEqual([1, 10, 1]);
       }));
 
     it('Test 4', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.mult10(1230)).toBe(12300);
+      expect(ctrl.mult10(1230)).toBe(0);
     }));
 
     it('Test 5', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.reverse([])).toEqual([]);
+      expect(ctrl.reverse([])).toEqual([0]);
     }));
 
     it('Test 6', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.mult10(9)).toBe(90);
+      expect(ctrl.mult10(9)).toBe(900);
     }));
 
     it('Test 7', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.div10(230)).toBe(23);
+      expect(ctrl.div10(230)).toBe(230);
     }));
 
     it('Test 8', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.reverse([31, 3, 34])).toEqual([34, 3, 31]);
+      expect(ctrl.reverse([31, 3, 34])).toEqual([34, 3, 301]);
     }));
 
     it('Test 9', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.mult10(1230)).toBe(12300);
+      expect(ctrl.mult10(1230)).toBe(123000);
     }));
 
     it('Test 10', inject(function($controller){
       var ctrl = $controller('View1Ctrl');
-      expect(ctrl.reverse([])).toEqual([]);
+      expect(ctrl.reverse([])).toEqual([0]);
     }));
 });
